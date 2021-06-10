79181158578786d917691a6f4880e7666516f70f
Manjunath Reddy <manju16832003@gmail.com>
2021-06-10 12:46:37 +0800

    Automatically switch to node version based on .nvmrc

--- switch-node-version.sh
+++ switch-node-version.sh
@@ -1,2 +1,2 @@
-// keep this alias in .bashrc or .zshrc file
+// keep this alias in .bashrc or .zshrc file. Make sure to have .nvmrc in your project directory
 alias cd='function cdnvm(){ cd $@; if [[ -f .nvmrc ]]; then <.nvmrc nvm install; fi; };cdnvm'
